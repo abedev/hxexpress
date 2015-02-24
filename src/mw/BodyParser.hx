6caf9f7b943dbeae3cbcb2637ba5986ecfc43b3a
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-24 10:27:21 -0700

    added BodyParser and options

---
+++ src/mw/BodyParser.hx
@@ -0,0 +1,12 @@
+package mw;
+
+import express.Middleware;
+import mw.bodyparser.*;
+
+@:jsRequire("body-parser")
+extern class BodyParser {
+  static function json(?options : JsonOptions) : Middleware;
+  static function raw(?options : RawOptions) : Middleware;
+  static function text(?options : TextOptions) : Middleware;
+  static function urlencoded(?options : RawOptions) : Middleware;
+}
