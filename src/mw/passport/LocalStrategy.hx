3205efd22f3927747ad5a7bb6707d6bd0ed3b8a9
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-05 23:03:19 -0700

    improved strategy type definitions

--- src/mw/passport/LocalStrategy.hx
+++ src/mw/passport/LocalStrategy.hx
@@ -3,6 +3,6 @@ package mw.passport;
 import js.Error;
 
 @:jsRequire("passport-local")
-extern class LocalStrategy {
-  function new(username : String, password : String, done : Error -> Dynamic -> Void) : Void;
+extern class LocalStrategy<T> {
+  function new(username : String, password : String, done : Error -> T -> Void) : Void;
 }
