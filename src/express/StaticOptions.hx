50baad061f16aa5f68646bf1d88779882ceea30d
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-05 22:58:51 -0700

    added StaticOptions fields

--- src/express/StaticOptions.hx
+++ src/express/StaticOptions.hx
@@ -1,5 +1,22 @@
 package express;
 
+import haxe.EitherType;
+import js.node.fs.Stats;
+
 typedef StaticOptions = {
-  
+  ?dotfiles : DotFile,
+  ?etag : Bool,
+  ?extensions : EitherType<Bool, Array<String>>,
+  ?index : EitherType<Bool, EitherType<String, Array<String>>,
+  ?lastModified : Bool,
+  ?maxAge : EitherType<String, Float>,
+  ?redirect : Bool,
+  ?setHeaders : Response -> String -> Stats -> Void
+}
+
+@:enum
+abstract DotFile(String) to String {
+  var allow = "allow";
+  var deny = "deny";
+  var ignore = "ignore";
 }
