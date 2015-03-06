6812e3be34e71ee631f4f16ed3ff06615ddc4a97
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-05 23:03:04 -0700

    cleanup

--- src/mw/bodyparser/RawOptions.hx
+++ src/mw/bodyparser/RawOptions.hx
@@ -7,8 +7,6 @@ import js.node.Buffer;
 
 typedef RawOptions = {
   ?inflate : Bool,
-  // in bytes https://www.npmjs.com/package/bytes
-  // TODO can use abstract
   ?limit : EitherType<String, Int>,
   ?type : EitherType<String, Request -> Bool>,
   ?verify : Request -> Response -> Buffer -> String -> Void
