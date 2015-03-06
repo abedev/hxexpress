645274cce27ea7d3408eebaeb0922e3f74da90f8
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-03-06 14:57:48 -0700

    Handy Next methods

--- src/express/Next.hx
+++ src/express/Next.hx
@@ -10,4 +10,12 @@ abstract Next(Dynamic)
   from FNext to FNext
   from FNextRoute to FNextRoute
   from FNextError to FNextError
-{ }
+{
+  public inline function call() {
+    untyped this();
+  }
+
+  public inline function error(err : js.Error) {
+    untyped this(err);
+  }
+}
