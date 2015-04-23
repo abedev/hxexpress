423a1144863c60c12c27faaa570077c073cea1d7
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-23 13:10:28 -0600

    added documentation

---
+++ generate.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+haxelib run dox -o docs -i xml/js.xml --title "Express Externs and Middleware for Haxe" -in mw -in express -theme flambe
+git archive -o docs/offline.zip HEAD docs
+rm -f docs/offline.zip
+zip -r docs/offline.zip docs -x "*/\.*"
