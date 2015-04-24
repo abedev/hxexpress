86c3b39feff9f57afac5b29e4b7489a43db0d8e8
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-24 10:20:07 -0600

    typo

--- src/mw/Busboy.hx
+++ src/mw/Busboy.hx
@@ -3,7 +3,7 @@ package mw;
 import express.Middleware;
 import mw.busboy.Options;
 
-@:jsRequire("conntect-busboy")
+@:jsRequire("connect-busboy")
 extern class Busboy {
   @:selfCall static function create(?options : Options) : Middleware;
 }
