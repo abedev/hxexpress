3618d52ff725f6ad4c7f35fb2c14ce71ec9836a0
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-04-03 17:13:38 -0600

    fixed Favicon module

--- src/mw/FavIcon.hx
+++ src/mw/FavIcon.hx
@@ -3,7 +3,7 @@ package mw;
 import haxe.extern.EitherType;
 import express.*;
 
-@:jsRequire("favicon")
+@:jsRequire("serve-favicon")
 extern class FavIcon {
   @:selfCall static function create(path : String, ?options : { maxAge : EitherType<Float, String> }) : Middleware;
 }
